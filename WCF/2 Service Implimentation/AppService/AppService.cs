11bb8b24b53d08e20969f9089ea41876746a7242
sureshyalagudri <30523699+sureshyalagudri@users.noreply.github.com>
2018-06-19 22:54:05 +0530

    Good to learn

---
+++ WCF/2 Service Implimentation/AppService/AppService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Entities;
+using Zza.Data;
+using System.ServiceModel;
+
+namespace AppService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    class AppService : IAppService, IDisposable
+    {
+        readonly ZzaDbContext dbContext = new ZzaDbContext();
+        public List<Product> GetProducts()
+        {
+            return dbContext.Products.ToList();
+        }
+        public List<Customer> GetCustomers()
+        {
+            return dbContext.Customers.ToList();
+        }
+
+        [OperationBehavior(TransactionScopeRequired=true)]
+        public void SubmitOrder(Order order)
+        {
+            dbContext.Orders.Add(order);
+            order.OrderItems.ForEach(oi=> dbContext.OrderItems.Add(oi));
+            dbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            dbContext.Dispose();
+        }
+    }
+}   
---
+++ WCF/2 Service Implimentation/AppService/IAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Entities;
+
+namespace AppService
+{
+    [ServiceContract]
+    public interface IAppService
+    {
+        [OperationContract]
+        List<Product> GetProducts();
+        [OperationContract]
+        List<Customer> GetCustomers();
+        [OperationContract]
+        void SubmitOrder(Order order);
+    }
+}
