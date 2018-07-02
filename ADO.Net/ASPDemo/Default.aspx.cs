1b14150fd47013cd87969b21ab40c4a623eb7a0a
Suresh <30523699+sureshyalagudri@users.noreply.github.com>
2018-07-02 16:51:46 +0530

    ADO

---
+++ ADO.Net/ASPDemo/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataLayer;
+using System.Data.SqlClient;
+
+namespace ASPDemo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                DataLayer.DB.ApplicationName = "ASPDemo Application";
+                DataLayer.DB.ConnectionTimeOut = 30;
+                SqlConnection conn = DataLayer.DB.GetsqlConnection();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ADO.Net/WinDemo/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLayer;
+using System.Data.SqlClient;
+
+namespace WinDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                DataLayer.DB.ApplicationName = "WinDemo Application";
+                DataLayer.DB.ConnectionTimeOut = 30;
+                SqlConnection conn = DataLayer.DB.GetsqlConnection();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
